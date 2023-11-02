0dbd7345c4f5b507b095676b389140cf1f4687ce
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-02 14:34:48 -0400

    Was able to finish the links for the header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,30 +1,21 @@
-// app/layout.tsx
+// src/app/components/Header/Header.tsx
 
 import React from "react";
 import Link from "next/link";
 import styles from "./Header.module.css";
+import Links from "../Links/Links";
 
-const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div>
-        <Link href={"/"}>
-          <img src="icon.png" alt="Icon" className={styles.icon} />
-        </Link>
-      </div>
-      <nav>
-        <Link href="/what-i-do" className={styles.links}>
-          What I Do
-        </Link>
-        <Link href="/what-im-building" className={styles.links}>
-          What I'm Building
-        </Link>
-        <Link href="/lets-chat" className={styles.links}>
-          Let's Chat
-        </Link>
-      </nav>
-    </header>
-  );
-};
+const Header = () => (
+  <header className={styles.header}>
+    <div>
+      <Link href={"/"}>
+        <img src="icon.png" alt="Icon" className={styles.icon} />
+      </Link>
+    </div>
+    <nav>
+      <Links />
+    </nav>
+  </header>
+);
 
 export default Header;
---
+++ src/app/components/Links/Links.tsx
@@ -0,0 +1,23 @@
+// src/app/components/Links/Links.tsx
+
+import React from "react";
+import Link from "next/link";
+import styles from "./Links.module.css";
+
+const Links = () => {
+  return (
+    <>
+      <Link href="/what-i-do" className={styles.links}>
+        What I Do
+      </Link>
+      <Link href="/what-im-building" className={styles.links}>
+        What I'm Building
+      </Link>
+      <Link href="/lets-chat" className={styles.links}>
+        Let's Chat
+      </Link>
+    </>
+  );
+};
+
+export default Links;
