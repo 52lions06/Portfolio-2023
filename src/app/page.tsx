5c5f416fd6fe3ca83ed347f51067ba8e61611262
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-02 22:21:53 -0400

    Cleaned up the home page CSS file and updated the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <main className={styles.main}>
-        <div></div>
+      <main>
+        <section>
+          <h1 className={styles.block}> Welcome to Will's World</h1>
+        </section>
+        <section className={styles.mainContent}>
+          <h2>
+            Text that I will be adding about who I am, why I had to create a new
+            portfolio, and what I set out todo{" "}
+          </h2>
+        </section>
+        <section></section>
       </main>
     </>
   );
