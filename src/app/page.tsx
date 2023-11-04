6a84c9b70c19a23ae9576abd22cca5a7d517da9b
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-03 23:19:09 -0400

    Text is stating to move around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,78 @@
+"use client";
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const blockRef = useRef<HTMLHeadingElement | null>(null);
+  const textRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const block = blockRef.current;
+    const text = textRef.current;
+
+    let blockWidth = 0;
+    let blockHeight = 0;
+    let textWidth = 0;
+    let textHeight = 0;
+
+    let x = 0;
+    let y = 0;
+    let xDirection = 1; // 1 for right, -1 for left
+    let yDirection = 1; // 1 for down, -1 for up
+    let isRotated = false;
+
+    const animateText = () => {
+      if (block && text) {
+        blockWidth = block.offsetWidth;
+        blockHeight = block.offsetHeight;
+        textWidth = text.offsetWidth;
+        textHeight = text.offsetHeight;
+      }
+
+      // Move the text in the x and y direction
+      x += xDirection;
+      y += yDirection;
+
+      // Rotate and change direction when hitting the borders
+      if (x > blockWidth - textWidth || x < 0) {
+        xDirection *= -1;
+        if (text) {
+          // Rotate 90 degrees if not already rotated, otherwise rotate to 0 degrees
+          text.style.transform = isRotated ? "rotate(0deg)" : "rotate(0deg)";
+          isRotated = !isRotated;
+        }
+      }
+      if (y > blockHeight - textHeight || y < 0) {
+        yDirection *= -1;
+        if (text) {
+          // Rotate 180 degrees if not already rotated, otherwise rotate to 0 degrees
+          text.style.transform = isRotated ? "rotate(0deg)" : "rotate(0deg)";
+          isRotated = !isRotated;
+        }
+      }
+
+      // Update the position of the text
+      if (text) {
+        text.style.left = `${x}px`;
+        text.style.top = `${y}px`;
+      }
+
+      requestAnimationFrame(animateText);
+    };
+
+    animateText();
+  }, []);
+
   return (
     <>
       <main>
         <section>
-          <h1 className={styles.block}> Welcome to Will's World</h1>
+          <h1 className={styles.block} ref={blockRef}>
+            {" "}
+            <span className={styles.title} ref={textRef}>
+              Welcome to Will's World
+            </span>
+          </h1>
         </section>
         <section className={styles.mainContent}>
           <h2>
