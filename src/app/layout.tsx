39ec66aa07a22ec7b1c1469eb164cd99e213077d
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-06 12:46:11 -0500

    I was able to update the background so the stars work and I can hover over <main> elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <Stars />
         {children}
+        <Stars />
         <Footer />
       </body>
     </html>
--- src/app/what-i-do/page.tsx
+++ src/app/what-i-do/page.tsx
@@ -1,10 +1,45 @@
+"use client";
 import React from "react";
-import styles from "./whatIDo.module.css";
+import styles from "./WhatIDo.module.css";
 
 const WhatIDo = () => {
+  const boxStyle = {
+    transition: "background-color 0.3s",
+    ":hover": {
+      backgroundColor: "#e0e0ff",
+    },
+  };
+
   return (
     <main>
       <h1>What I Do</h1>
+      <section className={styles.flexContainer}>
+        <div className={styles.box} style={boxStyle}>
+          <h2>I Manage</h2>
+          <p> People| Processes | Projects | Products</p>
+          <p> short description of what I am managing</p>
+        </div>
+        <div className={styles.box} style={boxStyle}>
+          <h2> I Make </h2>
+          <ul>
+            <li>Code</li>
+            <p>Javascript | JAVA | C++</p>
+            <li> Websites </li>
+            <p> Wix | Webflow | Wordpress </p>
+            <p> Maybe just replace with a short description of what I do</p>
+          </ul>
+        </div>
+        <div className={styles.box} style={boxStyle}>
+          <h2> I Markup </h2>
+          <p>
+            {" "}
+            short description stating that I have used confluence, atlassian,
+            Microsoft Office to document clear no nonesence processes and
+            workflows that are easy to understand implement and update when
+            necessary
+          </p>
+        </div>
+      </section>
     </main>
   );
 };
