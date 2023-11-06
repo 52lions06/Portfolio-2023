63e91ccc6114b3674a791a2e6ca9e3f98148eaa0
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-06 15:24:55 -0500

    Fixed footer and finished What I do page minus button that takes them to the lets chat page

--- src/app/what-i-do/page.tsx
+++ src/app/what-i-do/page.tsx
@@ -8,28 +8,47 @@ const WhatIDo = () => {
       <h1 className={styles.title}>What I Do</h1>
       <section className={styles.flexContainer}>
         <div className={`${styles.manage} ${styles.box}`}>
-          <h2>I Manage</h2>
-          <p> People| Processes | Projects | Products</p>
-          <p> short description of what I am managing</p>
+          <h2 className={styles.title}>I Manage</h2>
+          <b>
+            {" "}
+            Your Partner in People, Processes, Projects, and Products –
+            Delivering with Heart and Results
+          </b>
+          <p style={{ textAlign: "center" }}>
+            {" "}
+            In partnering with me, you'll find a commitment to compassionate
+            leadership and efficient management that uplifts teams and exceeds
+            goals. My approach delivers exceptional value to both people and
+            products, ensuring your success is met with integrity and meaningful
+            results.
+          </p>
         </div>
         <div className={`${styles.make} ${styles.box}`}>
-          <h2> I Make </h2>
-          <ul>
-            <li>Code</li>
-            <p>Javascript | JAVA | C++</p>
-            <li> Websites </li>
-            <p> Wix | Webflow | Wordpress </p>
-            <p> Maybe just replace with a short description of what I do</p>
-          </ul>
+          <h2 className={styles.title}> I Make </h2>
+          <b>
+            Tech Maestro: Coding Expertise and Integration Wizardry for
+            Next-Level Solutions
+          </b>
+          <p>
+            As a tech specialist who is commited to innovation, I excel in
+            problem-solving and programming across languages like JavaScript,
+            JAVA, and C++, using frameworks such as React, JQuery, and Kotlin. I
+            swiftly deploy and manage digital platforms using Webflow,
+            WordPress, and Wix.
+          </p>
         </div>
         <div className={`${styles.markup} ${styles.box}`}>
-          <h2> I Markup </h2>
+          <h2 className={styles.title}> I Markup </h2>
+          <b>
+            Documentation Dynamo: Mastering Clarity and Continuity in Workflow
+            Design
+          </b>
           <p>
-            {" "}
-            short description stating that I have used confluence, atlassian,
-            Microsoft Office to document clear no nonesence processes and
-            workflows that are easy to understand implement and update when
-            necessary
+            I specialize in creating comprehensive, user-friendly documentation
+            and workflows. My proficiency with tools like Atlassian's
+            Confluence, Microsoft Office suite, GitHub, and more, allows me to
+            create resources that are comprehensive, easily updatable, and able
+            to facilitate continuous improvement and knowledge sharing.
           </p>
         </div>
       </section>
