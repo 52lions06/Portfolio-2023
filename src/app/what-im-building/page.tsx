3f582100883207c03fa1f8ba7e556a3803e1d44f
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2023-11-11 01:51:36 -0500

    Finished Portfolio initially

--- src/app/what-im-building/page.tsx
+++ src/app/what-im-building/page.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import styles from "./WhatImBuilding.module.css";
 
 const WhatImBuilding = () => {
   return (
     <main>
       <h1>What I'm Building</h1>
+      <div className={styles.scene}>
+        <iframe
+          src="https://giphy.com/embed/iHD88spVFkL7mZakwa"
+          width="160"
+          height="160"
+          className={`${styles.circleiframe} ${styles.animate}`}
+        ></iframe>
+        <iframe
+          src="https://giphy.com/embed/cfGmVRsJI6wq6noGxP"
+          width="160"
+          height="160"
+          className={`${styles.circleiframe} ${styles.animate}`}
+          style={{ animationDelay: "3.3s" }}
+        ></iframe>
+        <iframe
+          src="https://giphy.com/embed/fVeAI9dyD5ssIFyOyM"
+          width="160"
+          height="160"
+          className={`${styles.circleiframe} ${styles.animate}`}
+          style={{ animationDelay: "6.6s" }}
+        ></iframe>
+      </div>
     </main>
   );
 };
